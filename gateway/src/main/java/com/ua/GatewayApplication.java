85165bc058c452b0732c8d6016c157926e87b45b
Валера <valerapetruna3107@gmail.com>
2021-02-08 16:12:31 +0200

    Containerize applications

--- gateway/src/main/java/com/ua/GatewayApplication.java
+++ gateway/src/main/java/com/ua/GatewayApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableEurekaClient
 @EnableZuulProxy
 public class GatewayApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
---
+++ init/01.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `bill_service_database`;
+CREATE DATABASE IF NOT EXISTS `deposit_service_database`;
