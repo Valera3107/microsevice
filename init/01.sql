09caa5c9fc372fe1b0b6c859c27fbe85b8b57e10
Валера <valerapetruna3107@gmail.com>
2021-02-09 11:59:58 +0200

    Fixed docker

--- init/01.sql
+++ init/01.sql
@@ -1,2 +1,4 @@
 CREATE DATABASE IF NOT EXISTS `bill_service_database`;
+GRANT ALL ON `bill_service_database`.* TO 'user'@'%';
 CREATE DATABASE IF NOT EXISTS `deposit_service_database`;
+GRANT ALL ON `deposit_service_database`.* TO 'user'@'%';
